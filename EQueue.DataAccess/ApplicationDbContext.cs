3a9b3f46b710ff142f9f5ad963556fd3d84d1d46
sunshine <thesunshineofficial@gmail.com>
2022-12-11 17:38:38 +0300

    Change architecture

--- EQueue.DataAccess/ApplicationDbContext.cs
+++ EQueue.DataAccess/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace EQueue.Data;
+namespace EQueue.DataAccess;
 
 public class ApplicationDbContext : IdentityDbContext
 {
--- EQueue/Program.cs
+++ EQueue/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using EQueue.Data;
+using EQueue.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
